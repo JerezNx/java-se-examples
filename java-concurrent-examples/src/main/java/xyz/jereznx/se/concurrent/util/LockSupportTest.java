b8d22e3f9360446edd4d67b2901cb5d6c8ce871f
JerezNx <772691544@qq.com>
2021-02-16 18:46:52 +0800

    增加LockSupport使用样例

---
+++ java-concurrent-examples/src/main/java/xyz/jereznx/se/concurrent/util/LockSupportTest.java
@@ -0,0 +1,28 @@
+package xyz.jereznx.se.concurrent.util;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * LockSupport 使用demo
+ *
+ * @author LQL
+ * @since Create in 2021/2/16 18:42
+ */
+public class LockSupportTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        final Thread thread = new Thread(() -> {
+            System.out.println("start");
+//            当前线程睡眠
+            LockSupport.park();
+            System.out.println("end");
+        });
+        thread.start();
+
+        TimeUnit.SECONDS.sleep(5);
+//        唤醒指定线程
+        LockSupport.unpark(thread);
+    }
+
+}
