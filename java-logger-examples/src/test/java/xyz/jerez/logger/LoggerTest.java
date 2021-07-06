c037f0280f0aa9a21a26fd63865d2bb78726008f
JerezNx <772691544@qq.com>
2021-07-06 20:27:48 +0800

    增加日志模块

---
+++ java-logger-examples/src/test/java/xyz/jerez/logger/LoggerTest.java
@@ -0,0 +1,18 @@
+package xyz.jerez.logger;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author liqilin
+ * @since 2021/3/13 13:35
+ */
+@Slf4j
+public class LoggerTest {
+
+    @Test
+    public void test() {
+        log.info("test");
+    }
+
+}
